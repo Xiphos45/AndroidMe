a2ee2894138191eba2e405691aa413fc01c0ea63
John Sawicki <jpsawicki@gmail.com>
2017-05-20 23:15:48 -0500

    main2activity

--- Main2Activity.java
+++ Main2Activity.java
@@ -2,9 +2,10 @@ package app.cominjohnsawickitrkhp_identity_photo.linkedin.httpswww.androidme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
-
+Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
